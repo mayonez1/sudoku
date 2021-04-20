a53d57c5849f8d249da9a0809aac6a2ab02173ed
mayonez1 <weeksbryson1@gmail.com>
2021-04-19 21:34:58 -0500

    WIP - see CHANGELOG

--- sudoku/src/driver.cpp
+++ sudoku/src/driver.cpp
@@ -1,7 +1,32 @@
-#include <square.hpp>
+//driver.cpp
 
-int main(){
-    Square test;
-    test.testRun();
-    return 0;
+#include <square.hpp>
+#include <menu.hpp>
+#include <types.hpp>
+#include <global.hpp>
+
+#include <string>
+
+using namespace std;
+
+int main(int argc, const char **argv){
+    string settingsPath = "settings/settings.txt";
+    SUDK::settings userSettings;
+    int skill;
+    string savePath;
+    Menu myMenu;
+    Square mySquare;
+
+
+    if (argc >= 1){
+        settingsPath = argv[0];
+    }
+
+    userSettings = GLOBAL::fetchSettings(settingsPath);
+
+    skill = userSettings.skill;
+    savePath = userSettings.savePath;
+    myMenu.init(settingsPath);
+    mySquare.init(skill, savePath);
+
 }
